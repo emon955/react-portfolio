d7dd26a003f8f21cc90d202138583ef6a04208bd
Sahariar <sahariaremon55@gmail.com>
2022-12-12 01:39:28 -0800

    start from nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import About from './Component/About';
+import Contact from './Component/Contact';
+import Home from './Component/Home';
+import Navbar from './Component/Navbar/Navbar';
+import SignUp from './Component/SignUp';
+import Skill from './Component/Skill';
 function App() {
   return (
-    <div className="App">
-        <h1>Nice to meet you</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="skill" element={<Skill />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
