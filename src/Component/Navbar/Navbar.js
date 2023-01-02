b408fb7f68da4626d72a57633e3901a9032d9145
Sahariar <sahariaremon55@gmail.com>
2023-01-02 02:48:18 -0800

    navbar responsive for mobile

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react'
-import {Link} from "react-router-dom";
-import { BsSearch } from 'react-icons/bs';
+import { Link } from "react-router-dom";
+import { BsSearch, BsHeart, BsMinecart } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+import { BiGitCompare } from 'react-icons/bi';
+
 import "./Navbar.css"
 const Navbar = () => {
-  const [isMobile,setMobile]=useState(false)
+  const [isMobile, setMobile] = useState(false)
   return (
     <nav className='navbar'>
-        <div className='logo'>
-           <Link to="/"><li><img src="https://flone.jamstacktemplates.dev/assets/img/logo/logo.png"/></li></Link>
-        </div>
-        <ul className={isMobile?"nav-links-mobile":"nav-links"}
-        onClick={()=> setMobile(false)}
-        >   <h1>hi</h1>
-            <Link to="/" className='home item'><li>Home</li></Link>
-            <Link to="/about" className='about item'><li>About</li></Link>
-            <Link to="/contact" className='contact item'><li>Contact</li></Link>
-             <Link to="/skill" className='skill item'><li>Skill</li></Link>
-            <Link to="/signup" className='signup item'><li>SignUp</li></Link>
-        </ul>
-        <div className='nav-right-wrapper'>
-           <BsSearch />
-        </div>
-        <button className='mobile-menu-icon'
-        onClick={()=> setMobile(!isMobile)}
-        >
-            {isMobile ? (<i className='fas fa-times'></i>):(<i className='fas fa-bars'></i>)}
-        </button>
+      <div className='logo'>
+        <Link to="/"><li><img src="https://flone.jamstacktemplates.dev/assets/img/logo/logo.png" /></li></Link>
+      </div>
+      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setMobile(false)}
+      >   <h1 className='close-nav'>hi</h1>
+        <Link to="/" className='home item'><li>Home</li></Link>
+        <Link to="/about" className='about item'><li>About</li></Link>
+        <Link to="/contact" className='contact item'><li>Contact</li></Link>
+        <Link to="/skill" className='skill item'><li>Skill</li></Link>
+        <Link to="/signup" className='signup item'><li>SignUp</li></Link>
+      </ul>
+      <div className='nav-right-wrapper'>
+        <button className='search-btn'><BsSearch /></button>
+        <button className='user-btn'><BiUser /></button>
+        <button className='compare-btn'><BiGitCompare /></button>
+        <button className='heart-btn'><BsHeart /></button>
+        <button className='cart-btn'><BsMinecart /></button>
+      </div>
+      <button className='mobile-menu-icon'
+        onClick={() => setMobile(!isMobile)}
+      >
+        {isMobile ? (<i className='fas fa-times'></i>) : (<i className='fas fa-bars'></i>)}
+      </button>
     </nav>
   )
 }
